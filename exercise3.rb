efc32fe027d9b3ca3a58bd999c59e7238976ef46
Shane <shanebeehler@me.com>
2016-08-09 14:38:55 -0400

    Get age. Puts birthyear.

--- exercise3.rb
+++ exercise3.rb
@@ -3,3 +3,11 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "Your name is #{name}."
+
+puts "How old are you?"
+
+age = gets.chomp
+
+year = 2016 - age.to_i
+
+puts "You were born in #{year}."
